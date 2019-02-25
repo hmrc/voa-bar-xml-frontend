eb27bbf096b38ae66d9e92591d36f31c2a31d1dd
Radek Hubner <rhubner@users.noreply.github.com>
2019-02-25 15:49:05 +0000

    Refactor configuration.
    Remove unused code.

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -39,10 +39,6 @@ class FrontendAppConfig @Inject() (override val runModeConfiguration: Configurat
   lazy val betaFeedbackUrl = s"$contactHost/contact/beta-feedback"
   lazy val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated"
 
-  lazy val authUrl = baseUrl("auth")
-  lazy val loginUrl = loadConfig("urls.login")
-  lazy val loginContinueUrl = loadConfig("urls.loginContinue")
-
   lazy val languageTranslationEnabled = runModeConfiguration.getBoolean("microservice.services.features.welsh-translation").getOrElse(true)
   def languageMap: Map[String, Lang] = Map(
     "english" -> Lang("en"),
