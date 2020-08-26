e14740b223ffb4529c4db1b11a226c99ed09c4c4
Valy Dia <2129101+valydia@users.noreply.github.com>
2020-08-26 11:33:13 +0100

    VOA-2242: Remove duplicate IDs in all scenarios

--- test/views/components/SubmissionPanelSpec.scala
+++ test/views/components/SubmissionPanelSpec.scala
@@ -46,31 +46,31 @@ class SubmissionPanelSpec extends ViewBehaviours  {
 
     "Contain a submission title equal to 'SUBMITTED' if the report status is submitted" in {
       lazy val doc = asDocument(submission1())
-      val status = doc.select("#summary-list > div:nth-child(1) > dd").text
+      val status = doc.select(s"#summary-list-${reportStatus1.id.split("-").head} > div:nth-child(1) > dd").text
       status mustBe messages("report.status.submitted.title")
     }
 
     "Contain a submission title equal to 'VERIFIED' if the report status is validated" in {
       lazy val doc = asDocument(submission2())
-      val status = doc.select("#summary-list > div:nth-child(1) > dd").text
+      val status = doc.select(s"#summary-list-${reportStatus2.id.split("-").head} > div:nth-child(1) > dd").text
       status mustBe messages("report.status.verified.title")
     }
 
     "Contain a submission title equal to 'FAILED' if the report status is invalidated" in {
       lazy val doc = asDocument(submission3())
-      val status = doc.select("#summary-list > div:nth-child(1) > dd").text
+      val status = doc.select(s"#summary-list-${reportStatus3.id.split("-").head} > div:nth-child(1) > dd").text
       status mustBe messages("report.status.failed.title")
     }
 
     "Contain a submission title equal to 'DONE' if the report status is forwarded" in {
       lazy val doc = asDocument(submission4())
-      val status = doc.select("#summary-list > div:nth-child(1) > dd").text
+      val status = doc.select(s"#summary-list-${reportStatus4.id.split("-").head} > div:nth-child(1) > dd").text
       status mustBe messages("report.status.done.title")
     }
 
     "Contain a submission title equal to 'PENDING' if the report status is forwarded" in {
       lazy val doc = asDocument(submission5())
-      val status = doc.select("#summary-list > div:nth-child(1) > dd").text
+      val status = doc.select(s"#summary-list-${reportStatus5.id.split("-").head} > div:nth-child(1) > dd").text
       status mustBe messages("report.status.pending.title")
     }
   }
