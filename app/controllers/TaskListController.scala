2bca58f0240a7bcec31d5934f7e15a1bfe4b723e
Valy Dia <2129101+valydia@users.noreply.github.com>
2020-11-04 16:35:02 +0000

    Minor tweak

--- app/controllers/TaskListController.scala
+++ app/controllers/TaskListController.scala
@@ -29,7 +29,7 @@ import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import utils.Navigator
 
 import scala.concurrent.ExecutionContext
-// $COVERAGE-OFF$
+
 class TaskListController @Inject()(appConfig: FrontendAppConfig,
                                    getData: DataRetrievalAction,
                                    requireData: DataRequiredAction,
@@ -56,4 +56,3 @@ class TaskListController @Inject()(appConfig: FrontendAppConfig,
   }
 
 }
-// $COVERAGE-ON$
